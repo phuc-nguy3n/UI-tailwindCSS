9de1b362015c99ce5d89b19747cb072b71103568
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-07-14 00:10:52 +0700

    Change class h-screen to h-full

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import UserProfile from "./One/UserProfile";
-import JobList from "./Two/JobList";
+// import JobList from "./Two/JobList";
+import Collection from "./Three/Collection";
 
 function App() {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-violet-50">
+    <div className="flex h-full w-full items-center justify-center bg-gray-300">
       {/* <UserProfile/> */}
-      <JobList />
+      {/* <JobList /> */}
+      <Collection />
     </div>
   );
 }
