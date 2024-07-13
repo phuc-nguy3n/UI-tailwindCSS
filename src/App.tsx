ca40a9496f4e8eaaff00bb5de09a4bd063dee0eb
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-07-13 14:43:32 +0700

    Comment UserProfile component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+// import UserProfile from "./One/UserProfile";
 import JobList from "./Two/JobList";
 
 function App() {
   return (
     <div className="flex h-screen w-full items-center justify-center bg-violet-50">
+      {/* <UserProfile/> */}
       <JobList />
     </div>
   );
