1d244820aca67f0d3c01aa15b97568599da3eec9
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-07-14 17:17:16 +0700

    Add transition color when user hover tag name

--- src/Components/Three/TagItem.tsx
+++ src/Components/Three/TagItem.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TagItem: React.FC<{ text: string }> = ({ text }): React.ReactElement => {
   return (
-    <div className="inline-block cursor-pointer rounded-lg bg-white p-2.5 font-popi font-medium text-slate-900">
+    <div className="inline-block cursor-pointer rounded-lg bg-white p-2.5 font-popi font-medium text-slate-900 transition-colors duration-500 ease-in-out hover:bg-slate-300 hover:text-zinc-50">
       {text}
     </div>
   );
