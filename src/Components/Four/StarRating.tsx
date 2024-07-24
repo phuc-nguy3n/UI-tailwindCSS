e30a6360b096b22fb4fd02414aa1e6a741c0a96a
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-07-24 22:32:37 +0700

    Maked StarIcon into StarRating Component

--- src/Components/Four/StarRating.tsx
+++ src/Components/Four/StarRating.tsx
@@ -1,6 +1,31 @@
 import { useState } from "react";
 
-const StarRating = (): React.ReactElement => {
+type IsFilledType = {
+  isFilled: boolean;
+};
+
+const StarIcon: React.FC<IsFilledType> = ({
+  isFilled,
+}: IsFilledType): React.ReactElement => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={`h-8 w-8 ${isFilled ? "fill-yellow-500 text-yellow-500" : "text-gray-400"}`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+      />
+    </svg>
+  );
+};
+
+const StarRating: React.FC = (): React.ReactElement => {
   const [rating, setRating] = useState<number | null>(null);
   const [hover, setHover] = useState<number | null>(null);
   return (
@@ -15,20 +40,7 @@ const StarRating = (): React.ReactElement => {
             onMouseEnter={() => setHover(star)}
             onMouseLeave={() => setHover(null)}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className={`h-8 w-8 ${isFilled ? "fill-yellow-500 text-yellow-500" : "text-gray-400"}`}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-              />
-            </svg>
+            <StarIcon isFilled={isFilled} />
           </div>
         );
       })}
