202aa8aa873489d453232073136d633a956052a4
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-07-24 22:30:58 +0700

    Import UserList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 // import UserProfile from "./One/UserProfile";
 // import JobList from "./Two/JobList";
 // import Collection from "./Three/Collection";
-import ReviewForm from "./Four/ReviewForm";
+// import ReviewForm from "./Four/ReviewForm";
+
+import UserList from "./Five/UserList";
 
 function App() {
   return (
-    <div className="flex h-full w-full items-center justify-center bg-gray-300">
+    <div className="flex h-screen w-full items-center justify-center bg-[#e6eaf8]">
       {/* <UserProfile/> */}
       {/* <JobList /> */}
       {/* <Collection /> */}
-      <ReviewForm />
+      {/* <ReviewForm /> */}
+      <UserList />
     </div>
   );
 }
